22ee697920f45a234a2b734123d90e3f72def253
stefamorenog <moreno.stefania17@gmail.com>
2025-10-13 22:42:19 -0500

    Se agrega dependencia para crear una navegacion escalable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("com.google.android.material:material:1.12.0")
     //Recorda datos
     implementation("androidx.datastore:datastore-preferences:1.1.1")
