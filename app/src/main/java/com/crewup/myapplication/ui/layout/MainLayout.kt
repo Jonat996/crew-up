4c89758f6c7a9058098c051f3c329a5859ef3fd2
jonat996 <jonathanmartinezc996@gmail.com>
2025-10-16 17:00:40 -0500

    feat: adding scroll to layout

--- app/src/main/java/com/crewup/myapplication/ui/layout/MainLayout.kt
+++ app/src/main/java/com/crewup/myapplication/ui/layout/MainLayout.kt
@@ -2,14 +2,20 @@ package com.crewup.myapplication.ui.layout
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun MainLayout(
     header: @Composable ColumnScope.() -> Unit,
     content: @Composable ColumnScope.() -> Unit
 ) {
-    Column {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier.verticalScroll(scrollState)
+    ) {
         header(this)
         content(this)
     }
